e5c77e672428f46577dc0bcaf3de5b64ed74db7b
Tiago Santos <tiagodinis33@gmail.com>
2020-07-02 09:20:42 +0100

    .

--- eclipse/RPC test/src/Main.java
+++
@@ -1,9 +0,0 @@
-import net.arikia.dev.drpc.DiscordEventHandlers;
-import net.arikia.dev.drpc.DiscordRPC;
-import net.arikia.dev.drpc.DiscordRichPresence;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
